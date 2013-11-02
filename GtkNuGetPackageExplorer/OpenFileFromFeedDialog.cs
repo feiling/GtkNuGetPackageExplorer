277229d1742156a3e726f040b1ae7e30b8b9c13d
feiling <feiling@microsoft.com>
2013-11-01 23:59:11 -0700

    Added prev page and next page buttons

--- GtkNuGetPackageExplorer/OpenFileFromFeedDialog.cs
+++ GtkNuGetPackageExplorer/OpenFileFromFeedDialog.cs
@@ -14,8 +14,11 @@ namespace GtkNuGetPackageExplorer
         private TreeView _packageList;
         private Label _info;
         private ListStore _store;
+        private IQueryable<IPackage> _packages;
 
         private const int _pageSize = 15;
+        private int _startCount;
+        private int _totalCount;
 
         public OpenFileFromFeedDialog()
             : base("Open from feed", null, DialogFlags.Modal)
@@ -32,16 +35,24 @@ namespace GtkNuGetPackageExplorer
             hbox.PackStart(_packageSource, expand: true, fill: true, padding: 5);
             
             var reloadButton = new Button("Reload");
-            reloadButton.Clicked += (o, e) => Reload();
+            reloadButton.Clicked += (obj, e) => Reload();
 
             hbox.PackStart(reloadButton, expand: false, fill: false, padding: 5);
             this.VBox.PackStart(hbox, expand: false, fill: false, padding: 5);
 
             hbox = new HBox();
-            _info = new Label();
-            hbox.PackStart(_info, expand: false, fill: false, padding: 5);
-            this.VBox.PackStart(hbox, expand: false, fill: false, padding: 5);
+            var button = new Button("Prev");
+            button.Clicked += (obj, e) => PrevPage();
+            hbox.PackStart(button, expand: false, fill: false, padding: 5);
 
+            _info = new Label();
+            hbox.PackStart(_info, expand: false, fill: false, padding: 5);            
+
+            button = new Button("Next");
+            button.Clicked += (obj, e) => NextPage();
+            hbox.PackStart(button, expand: false, fill: false, padding: 5);
+            this.VBox.PackStart(hbox, expand: false, fill: false, padding: 5);
+            
             _packageList = new TreeView();
             var column = new TreeViewColumn("Id", new CellRendererText(), "text", 0)
             {
@@ -83,6 +94,45 @@ namespace GtkNuGetPackageExplorer
             this.VBox.ShowAll();
         }
 
+        private void NextPage()
+        {
+            if (_packages == null || _startCount + _pageSize >= _totalCount)
+            {
+                return;
+            }
+
+            _startCount += _pageSize;
+            GetPackages();
+        }
+
+        private void PrevPage()
+        {
+            if (_packages == null || _startCount - _pageSize < 0)
+            {
+                return;
+            }
+
+            _startCount -= _pageSize;
+            GetPackages();
+        }
+
+        private void GetPackages()
+        {
+            _info.Text = string.Format("{0} to {1} of {2}", 
+                _startCount + 1,
+                _startCount + _pageSize,
+                _totalCount);
+            _store.Clear();
+            foreach (var p in _packages.Skip(_startCount).Take(_pageSize))
+            {
+                _store.AppendValues(
+                    p.Id,
+                    p.Version.ToString(),
+                    String.Join(", ", p.Authors),
+                    p.DownloadCount.ToString());
+            }
+        }
+
         private void Reload()
         {
             var source = _packageSource.Text;
@@ -101,17 +151,10 @@ namespace GtkNuGetPackageExplorer
             }
 
             var packageRepo = new DataServicePackageRepository(sourceUri);
-            var packages = packageRepo.GetPackages().OrderByDescending(p => p.DownloadCount);
-            var totalCount = packages.Count();
-            _info.Text = string.Format("total {0}", totalCount);
-            foreach (var p in packages.Take(_pageSize))
-            {
-                _store.AppendValues(
-                    p.Id,
-                    p.Version.ToString(),
-                    String.Join(", ", p.Authors),
-                    p.DownloadCount.ToString());
-            }
+            _packages = packageRepo.GetPackages().Where(p => p.IsAbsoluteLatestVersion == true).OrderByDescending(p => p.DownloadCount);
+            _startCount = 0;
+            _totalCount = _packages.Count();
+            GetPackages();
         }
     }
 }
